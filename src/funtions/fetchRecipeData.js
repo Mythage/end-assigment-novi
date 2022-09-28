de1b80523803838df5c5a02a758b6c013a5ea0dc
Mythage <so.nijdam@gmail.com>
2022-09-28 16:01:04 +0200

    JS start

---
+++ src/funtions/fetchRecipeData.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import createRecipeCard from "./createRecipeCard";
+
+// Try Fetching data from Edaman API
+export default async function fetchRecipeData(searchQuery, mealType, cuisineType, diet, time) {
+
+    //Declaration of input values for API
+    const URI = "https://api.edamam.com";
+    const ENDPOINT = "/api/recipes/v2";
+    const API_KEY = "d9ee381f552a7f704393ade7c82cffc0";
+    const API_ID = "305c6b1f";
+
+    // if succesfull then ...
+    try {
+        //fetch data from API
+        const response = await axios.get(URI + ENDPOINT,{
+            params: {
+                type:"public",
+                app_id: API_ID,
+                app_key: API_KEY,
+                q: searchQuery,
+                mealType: mealType,
+                cuisineType: cuisineType,
+                diet: diet,
+                time: time
+            }
+        } )
+
+        // Store recepi key in variable
+        const arrayOfRecipes = response.data.hits
+        // console.log(arrayOfRecipes);
+
+        createRecipeCard( arrayOfRecipes );
+
+        // Catch error massage and show them in the UI
+    } catch (e) {
+        const error = document.getElementById('error-message')
+
+        if (e.response.status === 404) {
+            //Execute page not found massage
+            error.innerContent = 'page not found'
+        } else if (e.response.status === 500) {
+            //Execute internal server error massage
+            error.innerContent = 'internal server error'
+        }
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,45 +1,30 @@
-import axios from "axios";
-import createRecipeCard from "./funtions/createRecipeCard";
 
-// Try Fetching data from Edaman API
-async function fetchRecipeData(searchQuery) {
+//Import from all created functions
+import fetchRecipeData from "./funtions/fetchRecipeData";
 
-    //Declaration of input values for API
-    const URI = "https://api.edamam.com";
-    const ENDPOINT = "/api/recipes/v2";
-    const API_KEY = "d9ee381f552a7f704393ade7c82cffc0";
-    const API_ID = "305c6b1f";
 
-    // if succesfull then ...
-    try {
-        //fetch data from API
-        const response = await axios.get(URI + ENDPOINT,{
-            params: {
-                type:"public",
-                app_id: API_ID,
-                app_key: API_KEY,
-                q: searchQuery
-            }
-        } )
+// Reference to form submit
+    const submitForm = document.getElementById('onSubmit');
 
-        // Store recepi key in variable
-        const arrayOfRecipes = response.data.hits
-        // console.log(arrayOfRecipes);
+    // Reference to input fields
+    const ingredients = document.getElementById('ingredients-field');
+    const mealType = document.getElementById('meal-type-field');
+    const cuisineType = document.getElementById('cuisine-type');
+    const dietType = document.getElementById('diet-type');
+    const time = document.getElementById('time');
 
-        createRecipeCard( arrayOfRecipes );
+    // send form with data
+submitForm.addEventListener('submit', (e) => {
+    // Prevent form to auto submit
+        e.preventDefault();
 
-        // Catch error massage and show them in the UI
-    } catch (e) {
-        const error = document.getElementById('error-message')
-
-        if (e.response.status === 404) {
-            //Execute page not found massage
-            error.innerContent = 'page not found'
-        } else if (e.response.status === 500) {
-            //Execute internal server error massage
-            error.innerContent = 'internal server error'
-        }
+        // Execute function to fetch data and parse out values as argument
+        fetchRecipeData(
+            ingredients.value,
+            mealType.value,
+            cuisineType.value,
+            dietType.value,
+            time.value
+        )
     }
-}
-
-fetchRecipeData("pasta");
+);
