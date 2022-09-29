52d43d550b47bcde3fa26b780367f2349d4efdd4
Mythage <so.nijdam@gmail.com>
2022-09-29 13:54:17 +0200

    cards done, only need to fix sizing/scaling cards in css

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -3986,11 +3986,12 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 function createRecipeCard(arr) {
     const recipeList = document.getElementById("recipe-card-list");
+    recipeList.innerHTML = "";
+    recipeList.setAttribute("class", "card--ulStyle inner-container full-width");
     arr.map((item)=>{
         // Create a list item for eatch recipe.
         const recipeItem = document.createElement("li");
         recipeItem.setAttribute("class", "card__main card--style");
-        // recipeItem.setAttribute('class', 'card--style');
         // craete a H3 element for recipe
         const recipeLabel = document.createElement("h3");
         recipeLabel.setAttribute("class", "recipe-lable");
@@ -4000,9 +4001,19 @@ function createRecipeCard(arr) {
         recipeImg.setAttribute("src", `${item.recipe.image}`);
         recipeImg.setAttribute("alt", `${item.recipe.label}`);
         recipeImg.setAttribute("class", "card--img");
+        // Create paragraf text in element
+        const recipeText = document.createElement("p");
+        recipeText.setAttribute("class", "card--label");
+        recipeText.textContent = `${Math.round(item.recipe.calories)} Calories  | ${item.recipe.ingredientLines.length} ingredients`;
+        // Create Time Paragraf in element
+        const recipeTime = document.createElement("p");
+        recipeTime.setAttribute("class", "card--time");
+        recipeTime.textContent = `${item.recipe.totalTime} min`;
         // Append li with h3 & img
         recipeItem.appendChild(recipeImg);
         recipeItem.appendChild(recipeLabel);
+        recipeItem.appendChild(recipeText);
+        recipeItem.appendChild(recipeTime);
         // Append ul with li
         recipeList.appendChild(recipeItem);
     });
