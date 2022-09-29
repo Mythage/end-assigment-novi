61226ca14e65325f892e335fb5c6998c4685412a
Mythage <so.nijdam@gmail.com>
2022-09-29 16:57:56 +0200

    basic fonts install and used

--- src/funtions/createRecipeCard.js
+++ src/funtions/createRecipeCard.js
@@ -5,11 +5,11 @@
     recipeList.setAttribute('class', 'card--ulStyle inner-container full-width')
      arr.map(( item ) => {
 
-         // Create a list item for eatch recipe.
+         // Create a list item for each recipe.
          const recipeItem = document.createElement('li');
-         recipeItem.setAttribute('class', 'card__main card--style');
+         recipeItem.setAttribute('class', 'card__main card--style card__text');
 
-         // craete a H3 element for recipe
+         // create a H3 element for recipe
          const recipeLabel = document.createElement('h3');
          recipeLabel.setAttribute('class', 'card--label');
          recipeLabel.textContent = `${ item.recipe.label }`
@@ -20,12 +20,12 @@
          recipeImg.setAttribute('alt', `${ item.recipe.label }`);
          recipeImg.setAttribute('class', 'card--img')
 
-         // Create paragraf text in element
+         // Create paragraph text in element
          const recipeText = document.createElement('p');
          recipeText.setAttribute('class', 'card--label');
          recipeText.textContent = `${Math.round( item.recipe.calories)} Calories  | ${item.recipe.ingredientLines.length} ingredients`
 
-         // Create Time Paragraf in element
+         // Create Time Paragraph in element
          const recipeTime = document.createElement('p');
          recipeTime.setAttribute('class', 'card--time');
          recipeTime.textContent = `${ item.recipe.totalTime} min`
